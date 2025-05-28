4e69482ab479095646b9cd13d1b13fbdfa40e0ef
Eugene Lukin <el4933877@gmail.com>
2025-05-28 12:18:57 +0300

    models and serializers changes

--- room_reservation/reservations/serializers.py
+++ room_reservation/reservations/serializers.py
@@ -1,6 +1,6 @@
+from django.db.models import Q
 from django.utils import timezone
 from rest_framework import serializers
-from rest_framework.fields import CurrentUserDefault
 from rooms.serializers import RoomSerializer
 from users.serializers import UserSerializer
 
@@ -23,10 +23,22 @@ class ReservationSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
 
-        if data["start"] > data["end"]:
-            raise serializers.ValidationError("end must occur after start", "start_before_end")
-        if timezone.now() > data["start"]:
-            raise serializers.ValidationError("start must be after now", "start_before_now")
+        if data.get("start") >= data.get("end"):
+            raise serializers.ValidationError({"non_field_errors": "End must occur after start."})
+
+        if timezone.now() > data.get("start"):
+            raise serializers.ValidationError({"start": "Start must be after the current time."})
+
+        overlapping = Reservation.objects.filter(room_id=data["room"]).filter(
+            Q(start__lt=data.get("end")) & Q(end__gt=data.get("start"))
+        )
+
+        if self.instance:
+            overlapping.exclude(id=self.instance.id)
+
+        if overlapping.exists():
+            raise serializers.ValidationError({"room": "This room is already reserved for this time"})
+
         return data
 
     class Meta:
